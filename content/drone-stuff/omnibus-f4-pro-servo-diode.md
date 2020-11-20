155deb3b984ae5ef1f5d1d2614e30bafdd77a453
Stavros Korokithakis <hi@stavros.io>
2020-11-20 21:29:02 +0200

    Updates

--- content/drone-stuff/omnibus-f4-pro-servo-diode.md
+++ content/drone-stuff/omnibus-f4-pro-servo-diode.md
@@ -6,4 +6,4 @@ insert_anchor_links = "right"
 +++
 To isolate the servo 5V rail, remove this diode:
 
-![a435bcae86912205b6fac41731285b8d.png](6d668e05d8a54580966b94a752f3b7db)
+![a435bcae86912205b6fac41731285b8d.png](../../6d668e05d8a54580966b94a752f3b7db.png)
--- joplinexport.py
+++ joplinexport.py
@@ -3,6 +3,7 @@ import re
 import sqlite3
 from collections import defaultdict
 from pathlib import Path
+from shutil import copy
 from shutil import rmtree
 from typing import Optional
 
@@ -25,19 +26,27 @@ class Note:
 
 class JoplinExporter:
     content_dir = Path("content")
+    static_dir = Path("static")
+    joplin_dir = Path.home() / ".config/joplin-desktop"
 
     def clean_content_dir(self):
         """Reset the content directory to a known state to begin."""
         rmtree(self.content_dir)
+        rmtree(self.static_dir)
         self.content_dir.mkdir()
+        self.static_dir.mkdir()
         with open(self.content_dir / "_index.md", mode="w") as outfile:
             outfile.write('+++\nredirect_to = "welcome"\n+++')
 
     def resolve_note_links(self, note: Note) -> str:
         def replacement(match):
-            note_id = match.group(1)
-            new_url = self.get_note_url_by_id(note_id)
-            return "](" + ("../../" + new_url if new_url else note_id) + ")"
+            item_id = match.group(1)
+            new_url = self.get_note_url_by_id(item_id)
+            if not new_url:
+                new_url = self.get_resource_url_by_id(item_id)
+                if not new_url:
+                    new_url = item_id
+            return f"](../../{new_url})"
 
         return re.sub(r"\]\(:/([a-f0-9]{32})\)", replacement, note.body)
 
@@ -48,16 +57,35 @@ class JoplinExporter:
             return None
         return note.get_url()
 
+    def get_resource_url_by_id(self, resource_id: str) -> Optional[str]:
+        """Return a resource's relative URL by its ID."""
+        resource = self.resources.get(resource_id)
+        if not resource:
+            return None
+        return resource_id + "." + resource[1]
+
+    def copy_resources(self):
+        """Copy all the resources to the output directory."""
+        for resource_id, resource in self.resources.items():
+            title, extension = resource
+            copy(
+                self.joplin_dir / "resources" / (resource_id + "." + extension),
+                self.static_dir,
+            )
+
     def read_data(self):
-        conn = sqlite3.connect(Path.home() / ".config/joplin-desktop/database.sqlite")
+        """Read the data from the Joplin database."""
+        conn = sqlite3.connect(self.joplin_dir / "database.sqlite")
         c = conn.cursor()
 
         # Create table
         c.execute("""SELECT id, title FROM folders;""")
         self.folders = {id: title for id, title in c.fetchall()}
 
-        c.execute("""SELECT id, title FROM resources;""")
-        self.resources = {id: title for id, title in c.fetchall()}
+        c.execute("""SELECT id, title, file_extension FROM resources;""")
+        self.resources = {
+            id: (title, file_extension) for id, title, file_extension in c.fetchall()
+        }
 
         c.execute("""SELECT id, parent_id, title, body FROM notes;""")
         self.notes = defaultdict(list)
@@ -70,18 +98,20 @@ class JoplinExporter:
         conn.close()
 
     def export(self):
+        self.read_data()
+
         folder_list = list(self.folders.items())
         # Sort "Welcome" last.
         folder_list.sort(
             key=lambda x: x[1].lower().strip() if x[1] != "Welcome" else "0"
         )
 
-        outdir = Path.cwd() / "content"
         self.clean_content_dir()
+        self.copy_resources()
 
         for counter, folder in enumerate(folder_list, start=1):
             folder_id, folder_title = folder
-            dir = outdir / slugify(folder_title)
+            dir = self.content_dir / slugify(folder_title)
             dir.mkdir(parents=True)
             with (dir / "_index.md").open(mode="w") as outfile:
                 outfile.write(
@@ -98,7 +128,9 @@ Select one of the sublinks on the left to see the notes in this section."""
                 sorted(self.notes[folder_id], key=lambda n: n.title)
             ):
                 print(f"Exporting {folder_title} - {note.title}...")
-                with (outdir / (note.get_url() + ".md")).open(mode="w") as outfile:
+                with (self.content_dir / (note.get_url() + ".md")).open(
+                    mode="w"
+                ) as outfile:
                     outfile.write(
                         f"""+++
 title = "{note.title}"
@@ -112,6 +144,4 @@ insert_anchor_links = "right"
 
 if __name__ == "__main__":
     print("Exporting Joplin database...")
-    exporter = JoplinExporter()
-    exporter.read_data()
-    exporter.export()
+    JoplinExporter().export()

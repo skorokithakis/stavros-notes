fc17f838e7046faf50ed30b4e31b028cc816cbaf
Stavros Korokithakis <hi@stavros.io>
2021-01-07 19:31:46 +0200

    Add build script and make checkboxes enabled

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+git clone https://github.com/getzola/book.git themes/book/
+rm -rf public/
+zola build
+
+# Make checkboxes editable, just in case the user wants to keep their own checklist.
+find public/ -name "*.html" -type f -exec sed -i 's/input disabled=""/input/g' {} +

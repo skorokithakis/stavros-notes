cfef81da36ad292ed152fff6b53313f551c6f9ef
Stavros Korokithakis <hi@stavros.io>
2021-02-04 00:46:53 +0200

    Only show menu by default on wide devices

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -euxo pipefail
 rm -rf themes/book/
 git clone https://github.com/getzola/book.git themes/book/
 
+sed -i '5i if (window.innerWidth < 900) { $menu.classList.toggle("menu-hidden"); $page.classList.toggle("page-without-menu"); }\n' themes/book/static/book.js
+
 rm -rf public/
 zola build
 
